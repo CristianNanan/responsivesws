9ed2bf0e9b6fbc8ce33336a48c8436feb52da001
CristianNanan <cristiannanan@gmail.com>
2015-09-04 21:21:45 -0400

    SWS

---
+++ sws_files/images/divcontacts.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+	<title>Student Web Services &#124; University of Toronto</title>
+	<link href="scripts/classes.css" rel="stylesheet" type="text/css"/>
+	<link href="scripts/buttons.css" rel="stylesheet" type="text/css"/>
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<?php include 'header.php'; ?>
+<div class="bottom-section">
+		<div class="rosi-about-section">
+			<div class="rosi-more-info-headline">Divisional Contacts for Students</div>
+					
+		<div class="separate-section">
+		<h3>Not Set Up for PIN Reactivation</h3>
+			<div>
+			If you have not set up your account for PIN reactivation and have forgotten your PIN and/or Student Number, please go to your Registrar's office or graduate department of the faculty/college/school in which you are currently registered or were most recently registered. These offices can help you. Here is a list of <a href="divcontacts.php">Divisional Contacts.</a></div>	
+	</div>		
+			<div class="separate-section">
+		
+		<h3>Three Tries to Log into the SWS</h3>
+			<div>
+			If you enter your PIN incorrectly three times in succession, your access to the Student Web Service will be blocked for security reasons. You may re-establish access by completing the PIN reactivation procedure or by going to your Registrar's office or graduate department of the faculty/college/school in which you are currently registered or were most recently registered to have your PIN reset.</div>	
+	</div>
+	</div>
+<?php include 'widget_rosi_login.php'; ?>
+<?php include 'widget_de_login.php'; ?>
+</div>
+
+<div class="footer-section">
+<?php include 'footer.php'; ?>
